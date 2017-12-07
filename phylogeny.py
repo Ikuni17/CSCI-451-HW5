3b08f86f97c64f9e54442dbf77b6e04b06ed4d98
BradWhite <white.brad17@gmail.com>
2017-12-07 11:58:14 -0700

    edit distance

--- phylogeny.py
+++ phylogeny.py
@@ -8,6 +8,7 @@ December 6, 2017
 # Interpreter: Python 3.6.3
 
 import numpy as np
+import sys
 
 
 def edit_distance(s, t):
@@ -23,3 +24,26 @@ def edit_distance(s, t):
 
 def build_d(sequences):
     d = np.ndarray(shape=(len(sequences), len(sequences[0])))
+
+
+def main():
+    file = 'sequences.txt'
+    sequences = []
+    with open(file, 'r') as seq:
+        length = 0
+        checkLen = 0
+        for line in seq:
+            line = line.strip()
+            length = len(line)
+            print(length)
+            if checkLen == 0:
+                checkLen = length
+            elif length != checkLen:
+                print('Sequences are not of same length, end program')
+                sys.exit()
+            line = list(line)
+            sequences.append(line)
+    for i in sequences:
+        print(i)
+
+main()
